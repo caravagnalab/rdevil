Fast and correct computation of hessian + new scheduler

inst/pydevil/pydevil/guide.py
@@ -1,17 +1,12 @@
-import pandas as pd
-import numpy as np
 import torch
-
 import pyro 
 import pyro.distributions as dist
 from torch.distributions import constraints
 
-from pydevil.utils import init_beta, init_theta
-
-
 def guide(input_matrix, 
           model_matrix, 
           UMI, 
+          beta_estimate,
           dispersion_priors,
           dispersion_variance,
           group_matrix = None, 
@@ -27,7 +22,7 @@ def guide(input_matrix,
     n_genes = input_matrix.shape[1]
     n_features = model_matrix.shape[1]
     
-    beta_estimate = init_beta(torch.log((input_matrix + 1e-5) / UMI.unsqueeze(1)), model_matrix)
+    # beta_estimate = init_beta(torch.log((input_matrix + 1e-5) / UMI.unsqueeze(1)), model_matrix)
     # theta_estimate = init_theta(input_matrix * 1.)
     theta_estimate = dispersion_priors
     beta_mean = pyro.param("beta_mean", beta_estimate, constraint=constraints.real)
@@ -45,9 +40,9 @@ def guide(input_matrix,
     if full_cov:
         beta_loc = pyro.param("beta_loc", (torch.eye(n_features, n_features).repeat([n_genes,1,1]) * init_loc), constraint=constraints.lower_cholesky)
     else:
-        beta_loc = pyro.param("beta_loc", torch.ones(n_genes, n_features) * init_loc, constraint=constraints.positive) 
+        beta_loc = pyro.param("beta_loc", torch.ones(n_genes, n_features) * init_loc, constraint=constraints.positive)
     
-    theta_p = pyro.param("theta_p", theta_estimate, constraint=constraints.positive) 
+    theta_p = pyro.param("theta_p", theta_estimate, constraint=constraints.positive)
     
     with pyro.plate("genes", n_genes, dim = -1):
         # pyro.sample("theta", dist.LogNormal(dispersion_priors, dispersion_variance))
@@ -69,6 +64,5 @@ def guide(input_matrix,
             pyro.sample("beta", dist.Normal(beta_mean.t(), beta_loc).to_event(1))
 
 
-        
 def guide_mle(*args, **kargs):
   pass

inst/pydevil/pydevil/interface.py
@@ -8,8 +8,10 @@ from pyro.infer import Trace_ELBO, JitTrace_ELBO, NUTS, MCMC
 from tqdm import trange
 
 from pydevil.model import model
+from pydevil.scheduler import myOneCycleLR
 from pydevil.guide import guide
-from pydevil.utils import prepare_batch, compute_disperion_prior
+from pydevil.utils import prepare_batch, compute_disperion_prior, init_beta
+from pydevil.utils_hessian import compute_hessians
 
 from sklearn.metrics.pairwise import rbf_kernel
 
@@ -32,14 +34,12 @@ def run_SVDE(
     full_cov = True, 
     prior_loc = 10,
     theta_bounds = (0., 1e16),
-    init_loc = 10,
-    threshold = 0,
+    init_loc = 10
 ):
-  
     if cuda and torch.cuda.is_available():
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
     else:
-        torch.set_default_tensor_type(t=torch.FloatTensor)
+        torch.set_default_tensor_type(t=torch.torch.FloatTensor)
         
     if batch_size > input_matrix.shape[0]:
         batch_size = input_matrix.shape[0]
@@ -50,92 +50,104 @@ def run_SVDE(
         
     lrd = gamma_lr ** (1 / steps)
     
-    input_matrix, model_matrix, UMI = torch.tensor(input_matrix).int(), torch.tensor(model_matrix).float(), torch.tensor(ncounts).float()
+    input_matrix, model_matrix, UMI = torch.tensor(input_matrix).int(), torch.tensor(model_matrix, dtype = torch.float32), torch.tensor(ncounts, dtype = torch.float32)
+    beta_estimate_matrix = init_beta(torch.log((input_matrix + 1e-5) / UMI.unsqueeze(1)), model_matrix)
 
     if group_matrix is not None:
         group_matrix = torch.tensor(group_matrix).float()
 
     if gene_specific_model_tensor is not None:
-        gene_specific_model_tensor = torch.tensor(gene_specific_model_tensor).float()
+        gene_specific_model_tensor = torch.tensor(gene_specific_model_tensor, dtype = torch.float32)
   
     if optimizer_name == "SGD":
         optimizer = pyro.optim.SGD({"lr": lr})
     elif optimizer_name == "Adam":
         optimizer = pyro.optim.Adam({"lr": lr})
+    elif optimizer_name == "OneCycleLR":
+        optimizer = pyro.optim.PyroOptim(myOneCycleLR, {'lr': lr, "lrd" : gamma_lr, "steps": steps})
     else:
         optimizer = pyro.optim.ClippedAdam({"lr": lr, "lrd" : lrd})
             
     elbo_list = [] 
+    beta_list = []
+    overdisp_list = []
 
     pyro.clear_param_store()
         
     svi = pyro.infer.SVI(model, guide, optimizer, pyro.infer.TraceGraph_ELBO())
 
     dispersion_priors, dispersion_var = compute_disperion_prior(X = input_matrix)
-    
-    if (threshold <= 0) :
-      t = trange(steps, desc='Bar desc', leave = True)
-      for _ in t:
-  
-          input_matrix_batch, model_matrix_batch, UMI_batch, group_matrix_batch, \
-          gene_specific_model_tensor_batch, kernel_input_batch =  \
-          prepare_batch(input_matrix, model_matrix, UMI, group_matrix,  \
-          gene_specific_model_tensor, kernel_input, batch_size = batch_size)
-  
-          loss = svi.step(input_matrix_batch, model_matrix_batch, UMI_batch, dispersion_priors, dispersion_var, group_matrix_batch,
-          gene_specific_model_tensor_batch, kernel_input_batch, full_cov = full_cov, 
-          prior_loc = prior_loc,
-          theta_bounds = theta_bounds,
-          init_loc = init_loc)       
-  
-          elbo_list.append(loss)
-          norm_ind = input_matrix_batch.shape[0] * input_matrix_batch.shape[1]
-          t.set_description('ELBO: {:.5f}  '.format(loss / norm_ind))
-          t.refresh()
-    else:
-        t = trange(steps, desc='Bar desc', leave = True)
-        k_windows = int(steps * .01)
-        previous_elbo = np.array([None for _ in range(k_windows)])
-        converged_iterations = 0
 
-        for i in t:
-            input_matrix_batch, model_matrix_batch, UMI_batch, group_matrix_batch, \
-            gene_specific_model_tensor_batch, kernel_input_batch =  \
-            prepare_batch(input_matrix, model_matrix, UMI, group_matrix,  \
-            gene_specific_model_tensor, kernel_input, batch_size = batch_size)
-    
-            loss = svi.step(input_matrix_batch, model_matrix_batch, UMI_batch, dispersion_priors, dispersion_var, group_matrix_batch,
-            gene_specific_model_tensor_batch, kernel_input_batch, full_cov = full_cov, 
-            prior_loc = prior_loc,
-            theta_bounds = theta_bounds,
-            init_loc = init_loc)       
-    
-            elbo_list.append(loss)
-            norm_ind = input_matrix_batch.shape[0] * input_matrix_batch.shape[1]
-            t.set_description('ELBO: {:.5f}  '.format(loss / norm_ind))
-            t.refresh()
+    prev_overdisp = torch.zeros_like(dispersion_priors)
+    prev_beta = torch.zeros([model_matrix.shape[1], input_matrix.shape[1]])
 
-            previous_elbo[i % k_windows] = loss
-            if i >= k_windows:
-                moving_avg_elbo = np.mean(previous_elbo)
-                moving_sd_elbo = np.std(previous_elbo)
-                
-                if (moving_sd_elbo / moving_avg_elbo) <= threshold:
-                    converged_iterations += 1
-                else:
-                    converged_iterations = 0
+    t = trange(steps, desc='Bar desc', leave = True)
+    for _ in t:
 
-                if converged_iterations == k_windows:
-                    break
+        input_matrix_batch, model_matrix_batch, UMI_batch, group_matrix_batch, \
+        gene_specific_model_tensor_batch, kernel_input_batch =  \
+        prepare_batch(input_matrix, model_matrix, UMI, group_matrix,  \
+        gene_specific_model_tensor, kernel_input, batch_size = batch_size)
+
+        loss = svi.step(input_matrix_batch, model_matrix_batch, UMI_batch, beta_estimate_matrix, dispersion_priors, dispersion_var, group_matrix_batch,
+        gene_specific_model_tensor_batch, kernel_input_batch, full_cov = full_cov, prior_loc = prior_loc, theta_bounds = theta_bounds, init_loc = init_loc)  
+
+        elbo_list.append(loss)
+        norm_ind = input_matrix_batch.shape[0] * input_matrix_batch.shape[1]
+        t.set_description('ELBO: {:.5f}  '.format(loss / norm_ind))
+        t.refresh()
+
+        curr_beta = pyro.param("beta_mean")
+        curr_overdisp = pyro.param("theta_p")
+
+        beta_list.append((100 * torch.mean(abs(curr_beta - prev_beta) / abs(prev_beta))).detach().cpu().numpy())
+        overdisp_list.append((100 * torch.mean(abs(curr_overdisp - prev_overdisp) / prev_overdisp)).detach().cpu().numpy())
+
+        prev_beta = torch.clone(curr_beta)
+        prev_overdisp = torch.clone(curr_overdisp)
     
-    n_features = model_matrix.shape[1]
+    # n_features = model_matrix.shape[1]
     coeff = pyro.param("beta_mean")
     overdispersion = pyro.param("theta_p")
 
-    if full_cov and n_features > 1:
-        loc = torch.bmm(pyro.param("beta_loc"),pyro.param("beta_loc").permute(0,2,1))
-    else:
-        loc = pyro.param("beta_loc")
+    # if full_cov and n_features > 1:
+    #    loc = torch.bmm(pyro.param("beta_loc"),pyro.param("beta_loc").permute(0,2,1))
+    #else:
+    #    loc = pyro.param("beta_loc")
+
+    # if full_cov and n_features > 1:
+    #     loc = torch.zeros(input_matrix.shape[1], n_features, n_features)
+    # else:
+    #     loc = torch.zeros(input_matrix.shape[1], n_features)
+
+    # t = trange(input_matrix.shape[1], desc='Bar desc', leave = True)
+    # for gene_idx in t:
+    #     beta = torch.tensor(coeff[:,gene_idx])
+    #     alpha = 1 / overdispersion[gene_idx]
+    #     obs = input_matrix[gene_idx,]
+    #     n_coefficients = model_matrix.shape[1]
+    #     H = torch.zeros([n_coefficients, n_coefficients])
+
+    #     for sample_idx in range(len(obs)):
+    #         yi = obs[sample_idx]
+    #         design_v = model_matrix[sample_idx,]
+    #         xij = torch.ger(design_v, design_v)
+    #         k = torch.exp(torch.dot(design_v, beta))
+    #         gamma_sq = (1 + alpha * k)**2
+            
+    #         H -= (yi * alpha + 1) * xij * k / gamma_sq
+        
+    #     solved_hessian = torch.inverse(-H)
+
+    #     if full_cov:
+    #         loc[gene_idx,:,:] = solved_hessian
+    #     else:
+    #         loc[gene_idx,:] = torch.diag(solved_hessian)
+
+    #     t.set_description('Variance estimation: {:.2f}  '.format(gene_idx / input_matrix.shape[1]))
+    #     t.refresh()
+
+    loc = compute_hessians(input_matrix=input_matrix, model_matrix=model_matrix, coeff=coeff, overdispersion=overdispersion, full_cov=full_cov)
 
     eta = torch.exp(torch.matmul(model_matrix, coeff) + torch.unsqueeze(torch.log(UMI), 1) )
     lk = dist.NegativeBinomial(logits = eta - torch.log(overdispersion) ,
@@ -159,14 +171,25 @@ def run_SVDE(
     variance =  eta + eta**2 / overdispersion
     # variance =  eta + eta**2 * overdispersion
 
-    ret = {"loss" : elbo_list, "params" : {
-        "theta" : overdispersion,
-        "lk" : lk,
-        "beta" : coeff,
-        "eta" : eta,
-        "variance" : loc
-        }, "residuals" : (input_matrix - eta) / np.sqrt(variance),
-        "hyperparams" : {"gene_names" : gene_names, "cell_names" : cell_names ,"model_matrix" : model_matrix}     }
+    ret = {
+        "loss" : elbo_list, 
+        "beta_list" : beta_list,
+        "overdisp_list" : overdisp_list,
+        "params" : {
+            "theta" : overdispersion,
+            "lk" : lk,
+            "beta" : coeff,
+            "eta" : eta,
+            "variance" : loc
+        }, 
+        "residuals" : (input_matrix - eta) / np.sqrt(variance),
+        "hyperparams" : {
+            "gene_names" : gene_names, 
+            "cell_names" : cell_names ,
+            "model_matrix" : model_matrix,
+            "full_cov" : full_cov
+        }     
+    }
 
     if group_matrix is not None:
         n_random = group_matrix.shape[1]
@@ -184,7 +207,8 @@ def run_SVDE(
             ret["params"]["lengthscale_kernel"] = pyro.param("lengthscale_param").cpu().detach().numpy()
         else:
             ret["params"]["lengthscale_kernel"] = pyro.param("lengthscale_param").detach().numpy()
-        
+            
+            
     return ret
   
   

inst/pydevil/pydevil/model.py
@@ -1,14 +1,11 @@
-import pandas as pd
-import numpy as np
 import torch
-
 import pyro 
 import pyro.distributions as dist
-from torch.distributions import constraints
 
 def model(input_matrix, 
           model_matrix, 
           UMI, 
+          beta_estimate,
           dispersion_priors,
           dispersion_variance,
           group_matrix = None, 
@@ -46,6 +43,7 @@ def model(input_matrix,
         zeta = pyro.sample("zeta", dist.MultivariateNormal(torch.zeros(n_genes, n_groups), scale_tril=torch.eye(n_groups, n_groups) * prior_loc, validate_args=False))
       else:
         zeta = pyro.sample("zeta", dist.Normal(torch.zeros(n_genes, n_groups), torch.ones(n_groups) * prior_loc).to_event(1))
+        
     if gene_specific_model_tensor is not None:
         n_features_gs = gene_specific_model_tensor.shape[1]
         beta_prior_mu_gs = torch.zeros(n_features_gs)
@@ -59,9 +57,9 @@ def model(input_matrix,
     else:
       beta = pyro.sample("beta", dist.Normal(beta_prior_mu, torch.ones(n_features) * prior_loc).to_event(1))
 
-    
     with pyro.plate("data", n_cells, dim = -2):
         eta = torch.matmul(model_matrix, beta.T)  + torch.log(UMI).unsqueeze(1)
+        
         if group_matrix is not None:
             eta_zeta = torch.matmul(group_matrix , zeta.T)
             eta = eta + eta_zeta
@@ -74,12 +72,8 @@ def model(input_matrix,
         # print(theta.min())
         #pyro.sample("obs", dist.GammaPoisson(rate = torch.clamp(torch.exp(eta + torch.log(1 / theta)),1e-9, 1e9) ,
         #concentration= torch.clamp(theta, 1e-9,1e9)), obs = input_matrix)
-
-        #pyro.sample("obs", dist.GammaPoisson(
-        #   concentration=torch.clamp(theta, 1e-9,1e9) , 
-        #   rate=torch.clamp(torch.exp(eta) / theta, 1e-9, 1e9)), 
-        #   obs=input_matrix)
         
-        pyro.sample("obs", dist.NegativeBinomial(logits = eta - torch.log(theta) ,
-        total_count= torch.clamp(theta, 1e-9,1e9)), obs = input_matrix)
+        # pyro.sample("obs", dist.GammaPoisson(concentration=theta, rate=theta / torch.exp(eta)), obs = input_matrix)
+        
+        pyro.sample("obs", dist.NegativeBinomial(logits = eta - torch.log(theta) , total_count=theta), obs = input_matrix)
     

inst/pydevil/pydevil/tests.py
@@ -3,27 +3,26 @@ from scipy.stats import norm
 from statsmodels.stats.multitest import fdrcorrection
 import pandas as pd
 
-
-
 def test_posterior_null(inference_res,contrast, alpha = 0.05):
-    mu_test = (inference_res["params"]["beta"] * contrast.reshape([-1,1])).sum(axis = 0)
-    
-    variance_term = (inference_res["params"]["variance"].diagonal(0,2,1) * contrast**2).sum(axis = 1)
+    full_cov = inference_res['hyperparams']['full_cov']
 
-    # covariance_term = 0
-    # if np.where(contrast > 0)[0] > 1:
-    #     total_terms = len(contrast)
-    #     for i in range(1, total_terms):
-    #         for j in range(0, i - 1):
-    #             covariance_term += contrast[i] * contrast[j] * inference_res["params"]["variance"][:,i,j]
+    if full_cov:
+        mu_test = (inference_res["params"]["beta"] * contrast.reshape([-1,1])).sum(axis = 0)
     
-    covariance_term = 0
-    total_terms = len(contrast)
-    for i in range(1, total_terms):
-        for j in range(0, i - 1):
-            covariance_term += contrast[i] * contrast[j] * inference_res["params"]["variance"][:,i,j]
+        variance_term = (inference_res["params"]["variance"].diagonal(0,2,1) * contrast**2).sum(axis = 1)
+        
+        covariance_term = 0
+        total_terms = len(contrast)
+        for i in range(1, total_terms):
+            for j in range(0, i - 1):
+                covariance_term += contrast[i] * contrast[j] * inference_res["params"]["variance"][:,i,j]
 
-    total_variance = variance_term + covariance_term
+        total_variance = (variance_term + covariance_term)
+    else:
+        mu_test = (inference_res["params"]["beta"] * contrast.reshape([-1,1])).sum(axis = 0)
+    
+        variance_term = (inference_res["params"]["variance"]**2 * abs(contrast)).sum(axis = 1)
+        total_variance = variance_term
 
     p_value = 2 * (1 - norm.cdf(np.abs(mu_test), scale = np.sqrt(total_variance)))
     is_significant, p_value_adj = fdrcorrection(p_value, alpha=alpha)

inst/pydevil/pydevil/utils.py
@@ -144,9 +144,10 @@ def compute_disperion_prior(X):
 
     if all_zero_column.any():
         num_zero_columns = all_zero_column.sum().item()
-        print(f"{num_zero_columns} columns contain too many zeros to calculate a size factor. A rough estimate will be used.")
+        print(f"{num_zero_columns} columns contain too many zeros to calculate a size factor. The size factor will be fixed to 1.")
         sf = sf / torch.exp(torch.log(sf).mean())
-        sf[all_zero_column] = (torch.sum(X.float(),dim=1) / (torch.sum(X.float(),dim=1).mean(dim=0)))[all_zero_column]
+        # sf[all_zero_column] = (torch.sum(X.float(),dim=1) / (torch.sum(X.float(),dim=1).mean(dim=0)))[all_zero_column]
+        sf[all_zero_column] = 1
     else:
         sf = sf / torch.exp(torch.log(sf).mean())
         
@@ -165,7 +166,11 @@ def compute_disperion_prior(X):
     def trend(x, a0, a1):
         return a0 / x**(1/2) + a1
 
-    fit_params, _ = curve_fit(trend, x, y, check_finite=False, bounds=((0, 0), (1e16, 1e16)))
+    try:
+        fit_params, _ = curve_fit(trend, x, y, check_finite=False, bounds=((0, 0), (1e16, 1e16)))
+    except:
+        return 1 / dispersion_estimate, torch.tensor(.25)
+    
     fitted_a0 = fit_params[0]
     fitted_a1 = fit_params[1]
 